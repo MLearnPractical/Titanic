4978ba128a9242bbf88bbe20bbdc8ea4778526fe
SithisCCCV <calculation@cox.net>
2018-01-02 21:32:42 -0700

    Hope you had fun getting Borged

---
+++ ESTest001.py
@@ -0,0 +1,5 @@
+print("It's Borgin' Time!")
+print("\n Nah, just kidding.")
+print("One, seven, three, four, six, seven, three, two, one, four, seven, six, Charlie, three, two, seven, eight, nine, seven, seven, seven, six, four, three, Tango, seven, three, two, Victor, seven, three, one, one, seven, eight, eight, eight, seven, three, two, four, seven, six, seven, eight, nine, seven, six, four, three, seven, six. Lock")
+number = 7
+print(number+7)
